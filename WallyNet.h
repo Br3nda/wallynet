53043cc7ab34819f55abb7c0c46751bb4b2bc324
Brenda Wallace <brenda@brenda.nz>
2016-01-01 14:49:31 +1300

    added reciever functions

--- WallyNet.h
+++ WallyNet.h
@@ -7,5 +7,8 @@
 #include <Arduino.h>
 
 void wallynet_send_beacon(String reading_name, String reading_value);
+void wallynet_listen();
+
 void wallynet_setup(int tx_pin, int led_pin, String station_name);
+void wallynet_setup_rx(int tx_pin, int led_pin);
 #endif
